2579b3a8479b1c24cc813402a688965b6e92f7fd
ivc6-10 <mirea@gmail.com>
2020-09-03 11:08:25 +0300

    eue

---
+++ src/Blass.java
@@ -0,0 +1,16 @@
+public class Blass {
+    public static void main (String[]args){
+        double i;
+        double s;
+        double k;
+        k=0;
+  for (i=1;i<11;i++)
+  {
+     s=1/i;
+     k+=s;
+      System.out.println(s);
+  }
+
+        System.out.println(k);
+    }
+}arr[1]
